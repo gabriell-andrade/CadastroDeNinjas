9aa8c1c40033a0a4fe132a79f51dc80bbc48b9bb
Gabriel Andrade <gabriel_anme@hotmail.com>
2025-04-09 20:40:23 -0300

    first commit

---
+++ src/main/java/br/com/gabrielandrade/cadastrodeninjas/CadastrodeninjasApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielandrade.cadastrodeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeninjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeninjasApplication.class, args);
+	}
+
+}
